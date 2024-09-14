1b0212b283dbba38f9f8814dce1b80820bf6d834
Michal Šrajer <michal.srajer@revolut.com>
2024-09-14 20:27:33 +0200

    redirect old db names

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,32 @@ export default {
   },
   methods: {
     async loadJSON() {
-      const db = new URLSearchParams(window.location.search).get('use') || 'hlgd';
+      const db = new URLSearchParams(window.location.search).get('use');
+      const defaultSearch = '?use=hlgd';
+      if (window.location.search === '') {
+        window.location.search = defaultSearch;
+      }
+      if (db.length > 4) {
+        // old db detected. Reload
+        this.$gtag.event('option_selected', {
+          event_category: 'old_db',
+          event_label: db,
+          value: 1,
+        });
+        if (db === 'ddh-lim77-overlap') window.location.search = '?use=hlod';
+        else if (db === 'ddh-nolim-overlap') window.location.search = '?use=hnod';
+        else if (db === 'ddh-lim77-nomerge') window.location.search = '?use=hlnd';
+        else if (db === 'ddh-nolim-nomerge') window.location.search = '?use=hnnd';
+        else if (db === 'ddh-lim77-chapter') window.location.search = '?use=hlgd';
+        else if (db === 'ddh-nolim-chapter') window.location.search = '?use=hngd';
+        else if (db === 'ddp-lim77-overlap') window.location.search = '?use=plod';
+        else if (db === 'ddp-nolim-overlap') window.location.search = '?use=pnod';
+        else if (db === 'ddp-lim77-nomerge') window.location.search = '?use=plnd';
+        else if (db === 'ddp-nolim-nomerge') window.location.search = '?use=pnnd';
+        else if (db === 'ddp-lim77-chapter') window.location.search = '?use=plgd';
+        else if (db === 'ddp-nolim-chapter') window.location.search = '?use=pngd';
+        else window.location.search = defaultSearch;
+      }
       console.log('DB:', db);
       let response;
       if (window.DecompressionStream) {
